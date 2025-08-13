bef5f47ab5c952389c950acee2461a691087f85e
ferb-ferb <ajferber@gmail.com>
2025-08-13 15:56:51 -0400

    Fixing executable (CMake changes)

---
+++ build/cpp_pubsub/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ferb/ros2-ws/src/cpp_pubsub
+# Build directory: /home/ferb/ros2-ws/build/cpp_pubsub
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
